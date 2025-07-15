47f44f70c4a03b81803fd04f11e1a41de5dbb892
Andrés <andrescalvo01@gmail.com>
2025-07-15 10:22:57 +0200

    commit 1

--- src/CalculateDNITest.java
+++ src/CalculateDNITest.java
@@ -7,16 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculateDNITest {
     @ParameterizedTest
     @CsvSource({
-            "12345678,Z",  // 12345678 % 23 = 14 → 'Z'
-            "11111111,H",  // 11111111 % 23 = 18 → 'H'
-            "22222222,M",  // 22222222 % 23 = 5  → 'M'
-            "33333333,B",  // 33333333 % 23 = 11 → 'B'
-            "44444444,Y",  // 44444444 % 23 = 6  → 'Y'
-            "55555555,R",  // 55555555 % 23 = 1  → 'R'
-            "66666666,D",  // 66666666 % 23 = 9  → 'D'
-            "77777777,N",  // 77777777 % 23 = 12 → 'N'
-            "88888888,L",  // 88888888 % 23 = 19 → 'L'
-            "99999999,J",  // 99999999 % 23 = 13 → 'J'
+            "12345678,Z",
+            "11111111,H",
+            "22222222,J",
+            "33333333,P",
+            "44444444,A",
+            "55555555,K",
+            "66666666,Q",
+            "77777777,B",
+            "88888888,Y",
+            "99999999,R",
     })
     void testCalculateDNILetter(int numberDNI, char expectedLetter) {
         assertEquals(expectedLetter, CalculateDNI.calculateLetter(numberDNI));
